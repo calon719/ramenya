e58535a48475e1c6e3923dbe48425e6c1665b452
Calon <calonhu@protonmail.com>
2022-03-31 01:25:08 +0800

    修正 UserOrderConfirm.vue 沒有 loading 動畫

--- src/views/UserOrder.vue
+++ src/views/UserOrder.vue
@@ -4,6 +4,30 @@
       <h2 class="page-banner-title fs-1">訂單資訊</h2>
     </header>
 
-    <router-view></router-view>
+    <router-view v-if="$route.name === 'UserOrderConfirm'"
+      @loadingStatus="toggleLoading"></router-view>
+    <router-view v-else></router-view>
+
+    <loading :isLoading="isLoading"></loading>
   </div>
 </template>
+
+<script>
+import loading from '@/components/LoadingComponent.vue';
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    toggleLoading(status) {
+      this.isLoading = status;
+    },
+  },
+  components: {
+    loading,
+  },
+};
+</script>
